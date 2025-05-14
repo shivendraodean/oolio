06e86ed4cc4d9c314de1bbfe65fded95fc244b58
Shivendra Odean <shivendra.odean@gmail.com>
2025-05-14 22:34:26 +1200

    setup: initialise main file and add gin

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hey, I am Healthy!",
+		})
+	})
+
+	router.Run(":8080")
 }
