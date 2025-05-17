38d3c2f4332c8291e2d63923c64c948cd79d7b21
Shivendra Odean <shivendra.odean@gmail.com>
2025-05-17 22:27:11 +1200

    feat: add order handler

---
+++ src/internal/webapp/handler/order_handler.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"net/http"
+	"oolio/api-ecommerce/src/internal/model"
+	"oolio/api-ecommerce/src/internal/service"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OrderHandler struct {
+	OrderService service.OrderService
+}
+
+func NewOrderHandler(orderService service.OrderService) *OrderHandler {
+	return &OrderHandler{OrderService: orderService}
+}
+
+func (h *OrderHandler) PlaceOrder(c *gin.Context) {
+	orderRequest, err := ValidatePlaceOrderRequest(c)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid order request"})
+
+		return
+	}
+
+	var items []model.OrderItem
+	for _, item := range orderRequest.Items {
+		productID, _ := strconv.ParseInt(item.ProductID, 10, 64)
+		items = append(items, model.OrderItem{ProductID: productID, Quantity: item.Quantity})
+	}
+
+	order, err := h.OrderService.PlaceOrder(items)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"An error occurred": err.Error()})
+
+		return
+	}
+
+	c.JSON(http.StatusCreated, order)
+}
--- src/internal/webapp/handler/request_validator.go
+++ src/internal/webapp/handler/request_validator.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Products
+
 type ProductRequest struct {
 	ID int64 `uri:"productId" binding:"required,min=1"`
 }
@@ -19,4 +21,27 @@ func ValidateGetProductRequest(c *gin.Context) (int64, error) {
 	}
 
 	return request.ID, nil
+}
+
+// Orders
+
+type OrderItemRequest struct {
+	ProductID string `json:"productId" binding:"required"`
+	Quantity  int    `json:"quantity" binding:"required"`
+}
+
+type PlaceOrderRequest struct {
+	CouponCode string             `json:"couponCode" binding:"omitempty,min=8,max=10"`
+	Items      []OrderItemRequest `json:"items" binding:"required,dive"`
+}
+
+func ValidatePlaceOrderRequest(c *gin.Context) (PlaceOrderRequest, error) {
+	var request PlaceOrderRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		slog.Error("Invalid place order request", "error", err.Error())
+		return PlaceOrderRequest{}, err
+	}
+
+	return request, nil
 }
